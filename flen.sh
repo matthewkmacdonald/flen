7bedec97784e05314ccc886666fa3fd1ef5e6cf1
Matthew MacDonald <mmacdonald@pantelintl.com>
2017-02-02 21:18:50 -0800

    Improved performance dramatically by switching to grep.  Added option to calculate function depth

--- flen.sh
+++ flen.sh
@@ -2,85 +2,133 @@
 
 # flen - the function length counter
 # counts the number of lines between two { } matching pairs
-# arg 1: Numeric limit over which to report
-# arg 2: where to look (path)
-# This will look in *.c *.cpp *.h *.php files
+# This can look in *.c *.cpp *.h *.php files, or any other language
+# that uses braces for scope.  It is very primitive and will not
+# detect #ifdefs, or commented code, so may give false positives in
+# some situations, so proceed with caution
 #
 # Written By: Matthew MacDonald
 # Date: 02-Feb-2017
 
-BLOCKSTART="{"
-BLOCKEND="}"
 
 # CountBraces
 # Matches up pairs of { }
-# arg 1: the file name
-# arg 2: Optional numerical limit.  This will echo the function
 # length when this limit is exceeded.  If no limit is provided
 # then all function lengths will be printed.  You probably don't
 # want to do that.
 function CountBraces() {
 
-theLineNumber=0
 j=0
 i=0
 theFileName=$1
+theLengthLimit=$2
+theDepthLimit=$3
 
-if [ -z $2 ]
-then
-	theOptionalLineLimit=0
-else
-	theOptionalLineLimit=$2
-fi
+aListOfBraces=`grep -on '\{\|\}' $theFileName`
 
-
-while read line
+for line in $aListOfBraces
 do
-	for (( i=0; i<${#line}; i++ )); do
-		if [ "${line:$i:1}" = "$BLOCKSTART" ]
+	IFS=':' read -ra anArrayOfBraces <<< $line
+	aLineNumber=${anArrayOfBraces[0]}
+	aBraceCharacter=${anArrayOfBraces[1]}
+	#echo " testing $aLineNumber $aBraceCharacter"
+
+	if [ "$aBraceCharacter" = "{" ]
+	then
+		arr[$j]=$aLineNumber
+		((j++))
+		if [ $j -gt $theDepthLimit ]
 		then
-			arr[$j]=$theLineNumber
-			((j++))
-		elif [ "${line:$i:1}" = "$BLOCKEND" ]
-		then
-			((j--))
-			if [ $j -lt 0 ]
-			then
-				echo "$theFileName:$theLineNumber error"
-				return
-			fi
-			x=${arr[$j]}
-			theFunctionLength=$((theLineNumber-x))
-			if [ $theFunctionLength -gt $theOptionalLineLimit ]
-			then
-				echo "$theFileName:$x $theFunctionLength"
-			fi
+			echo "(depth) $theFileName:$aLineNumber $j"
 		fi
-	done
-	((theLineNumber++))
-done < $1
+	elif [ "$aBraceCharacter" = "}" ]
+	then
+		((j--))
+		if [ $j -lt 0 ]
+		then
+			echo "(error) $theFileName:$aLineNumber"
+			return
+		fi
+		x=${arr[$j]}
+		aFunctionLength=$((aLineNumber-x))
+		if [ $aFunctionLength -gt $theLengthLimit ]
+		then
+			echo "(length) $theFileName:$x $aFunctionLength"
+		fi
+	fi
+done
 }
 
 
-################################### MAIN #####################################
-aFileType=$3
-aPath=$2
-aLimit=$1
-aListOfFiles=`find $aPath -type f -name "$aFileType"`
-
-if [ $# -lt 3 ]
-then
-	echo ""
+function PrintHelp() {
+        echo ""
 	echo "flen - the function length counter"
-	echo "usage: flen <length> <path> <file type>"
-	echo "example: ./flen.sh 400 . *.c"
+	echo "usage: flen -l <length> -d <depth> -p <path> -t <type>"
+	echo "Where:"
+	echo "	-l,--length	lines of code between opening and closing braces"
+	echo "	-d,--depth	Number of successive opening braces"
+	echo "	-p,--path	Path to files in which to search"
+	echo "	-t,--type	type of files in which to inspect"
+	echo ""
+	echo "example: ./flen.sh -l 400 -d 10 -p . -t *.c"
 	echo ""
 	echo "Sometimes you may wish to sort the output of flen:"
-	echo "./flen.sh 400 . *.c | sort -k2nr" 
+	echo "./flen.sh -l 400 -d 10 -p . -t *.c | sort -k3nr" 
+}
+
+################################### MAIN #####################################
+
+
+# flen -l <function length> -d <depth> -p <path> -t <file type>
+DEPTH=999
+LENGTH=999999
+
+# Parse Command Line Arguments
+while [[ $# -gt 1 ]]
+do
+	key="$1"
+
+	case $key in
+		-d|--depth)
+		DEPTH="$2"
+		shift # past argument
+		;;
+		-p|--path)
+		SEARCHPATH="$2"
+		shift # past argument
+		;;
+		-l|--length)
+		LENGTH="$2"
+		shift # past argument
+		;;
+		-t|--type)
+		FILETYPE="$2"
+		;;
+		*)
+		# unknown option
+		PrintHelp
+		exit 1
+		;;
+	esac
+shift # past argument or value
+done
+
+if [ -z $SEARCHPATH ]
+then
+	echo "Please provide a path in which to search"
+	PrintHelp
+	exit 1
+elif [ -z $FILETYPE ]
+then
+	echo "Please provide a file type to inspect"
+	PrintHelp
 	exit 1
 fi
 
+
+aListOfFiles=`find $SEARCHPATH -type f -name "$FILETYPE"`
+
 for file in $aListOfFiles; do
-	CountBraces $file $aLimit
+	CountBraces $file $LENGTH $DEPTH
 done
 ################################### EOF ######################################
